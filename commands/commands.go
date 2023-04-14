df1c50703fbb076823beeed080127e20d2846bbb
Eugene Matsumura <eugene.bb1@gmail.com>
2023-04-14 13:40:25 +0200

    feat: :sparkles: add docker build

--- commands/commands.go
+++ commands/commands.go
@@ -14,7 +14,7 @@ type Command struct {
 }
 
 func createCommand(cmd *discordgo.ApplicationCommand, execute func(s *discordgo.Session, i *discordgo.InteractionCreate)) {
-	log.Println("Creating command", cmd.Name)
+	log.Printf("Creating command: '%v'", cmd.Name)
 	CommandMap[cmd.Name] = Command{
 		AppCmd:  cmd,
 		Execute: execute,
@@ -25,7 +25,7 @@ func RegisterCommands(dg *discordgo.Session) {
 	for _, c := range CommandMap {
 		_, err := dg.ApplicationCommandCreate(dg.State.User.ID, "", c.AppCmd)
 		if err != nil {
-			log.Panicf("Cannot create '%v' command: %v", c.AppCmd.Name, err)
+			log.Panicf("Failed to register '%v' command: %v", c.AppCmd.Name, err)
 		}
 	}
 }
@@ -34,7 +34,8 @@ func UnregisterCommands(dg *discordgo.Session) {
 	for _, c := range CommandMap {
 		err := dg.ApplicationCommandDelete(dg.State.User.ID, "", c.AppCmd.ID)
 		if err != nil {
-			log.Printf("Could not delete '%v' command: %v", c.AppCmd.Name, err)
+			log.Printf("Failed to unregister '%v' command: %v", c.AppCmd.Name, err)
+			return
 		}
 	}
 }
